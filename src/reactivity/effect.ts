181d013c474940dfa44c954c01970015539b7f06
yuangenbao <769318433@qq.com>
2022-07-08 00:44:21 +0800

    初始化开发环境&实现reactive&effect&收集依赖&触发依赖

---
+++ src/reactivity/effect.ts
@@ -0,0 +1,42 @@
+class ReactiveEffect {
+    private _fn: any
+    constructor(fn) {
+        this._fn = fn
+    }
+    run() {
+        activeEffect = this
+        this._fn()
+    }
+}
+
+let activeEffect
+
+const targetMap = new Map()
+
+// 依赖收集器
+export const track = (target, key) => {
+    // target -> keys -> deps
+    if (!targetMap.get(target)) {
+        targetMap.set(target, new Map())
+    }
+    const depsMap = targetMap.get(target)
+
+    if (!depsMap.get(key)) {
+        depsMap.set(key, new Set())
+    }
+    const deps = depsMap.get(key)
+    deps.add(activeEffect)
+}
+
+// 依赖触发器
+export const trigger = (target, key) => {
+    const depsMap = targetMap.get(target)
+    const deps = depsMap.get(key)
+    for (const effect of deps) {
+        effect.run()
+    }
+}
+export const effect = fn => {
+    const _effect = new ReactiveEffect(fn)
+    _effect.run()
+}
