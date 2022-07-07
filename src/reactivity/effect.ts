2862179b84d8ea97c90a144c1050817abd3dac22
yuangenbao <769318433@qq.com>
2022-07-08 00:56:57 +0800

    effect runner

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -5,7 +5,7 @@ class ReactiveEffect {
     }
     run() {
         activeEffect = this
-        this._fn()
+        return this._fn()
     }
 }
 
@@ -39,4 +39,5 @@ export const trigger = (target, key) => {
 export const effect = fn => {
     const _effect = new ReactiveEffect(fn)
     _effect.run()
+    return _effect.run.bind(_effect)
 }
