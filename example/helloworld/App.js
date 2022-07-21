21a853097404753617b9319200670c99aff5daae
yuangenbao <769318433@qq.com>
2022-07-21 22:34:15 +0800

    初始化element主流程

--- example/helloworld/App.js
+++ example/helloworld/App.js
@@ -2,7 +2,15 @@ import { h } from '../../lib/i-mini-vue.esm.js'
 export const App = {
     render() {
         // ui
-        return h('div', 'helle ' + this.msg)
+        return h(
+            'div',
+            {
+                id: 'root',
+                class: ['red', 'hard'],
+            },
+            // 'helle ' + this.msg
+            [h('p', { class: 'red' }, '你好， mini-vue'), h('p', { class: 'blue' }, 'hello mini-vue')]
+        )
     },
     setup() {
         return {
--- src/runtime-core/render.ts
+++ src/runtime-core/render.ts
@@ -8,8 +8,11 @@ function patch(vnode, container) {
     // 处理组件
     // 判断是不是 elment 处理
     // 如果是Element调用
-    // processElement()
-    processComponent(vnode, container)
+    if (typeof vnode.type === 'string') {
+        processElement(vnode, container)
+    } else {
+        processComponent(vnode, container)
+    }
 }
 function processComponent(vnode: any, container: any) {
     // 挂载
@@ -27,6 +30,36 @@ function setupRenderEffect(instance: any, container) {
     // vnode tree
     // vnode ——>patch
     // vnode ——>element ->mountElement
-
     patch(subTree, container)
+}
+
+function processElement(vnode, container) {
+    mountElement(vnode, container)
+}
+
+function mountElement(vnode, container) {
+    const el = document.createElement(vnode.type)
+    const { children, props } = vnode
+
+    if (typeof children === 'string') {
+        el.textContent = children
+    } else if (Array.isArray(children)) {
+        mounteChildren(vnode, el)
+    }
+
+    for (const key in props) {
+        const val = props[key]
+        if (typeof val === 'string') {
+            el.setAttribute(key, val)
+        } else if (Array.isArray(val)) {
+            el.setAttribute(key, val.join(' '))
+        }
+    }
+    container.appendChild(el)
+}
+
+function mounteChildren(vnode, container) {
+    vnode.children.forEach(v => {
+        patch(v, container)
+    })
 }
